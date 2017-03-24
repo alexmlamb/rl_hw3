c98d84d810e1c55e1c66730a6bb0cb1f823cc561
Alexander Lamb <lambalex@leto07.iro.umontreal.ca>
2017-03-23 22:30:39 -0400

    updates

--- lib/main.py
+++ lib/main.py
@@ -28,9 +28,15 @@ Actual learning:
 
 Take action on each step by selecting high value from Q-network.  
 
+---------
+
 '''
 
 import random
+import theano
+import theano.tensor as T
+from layers import param_init_fflayer, fflayer
+import numpy as np
 
 '''
 sales[t] to Inventory[t+1]
@@ -47,14 +53,83 @@ Sales[t+1] from Inventory[t].
 def basic_environment(inv):
     return min(inv, 10)
 
+def init_params(p):
 
-sales = 10.0
+    p = param_init_fflayer({},p,prefix='qn_1',nin=2,nout=512)
+    p = param_init_fflayer({},p,prefix='qn_2',nin=512,nout=512)
+    p = param_init_fflayer({},p,prefix='qn_3',nin=512,nout=1)
 
-for iteration in range(0,50):
-    inv = basic_policy(sales)
-    sales = basic_environment(inv)
+    p = param_init_fflayer({},p,prefix='mqn_1',nin=1,nout=512)
+    p = param_init_fflayer({},p,prefix='mqn_2',nin=512,nout=512)
+    p = param_init_fflayer({},p,prefix='mqn_3',nin=512,nout=1)
 
-    print inv,sales
+    return p
+
+'''
+    State and action to q-value.  
+    In this context these are sales,inventory
+    sales is an (Nx1) matrix, inventory is an (Nx1) matrix.  
+'''
+def qnetwork(p,sales,inventory):
+    inp = T.concatenate([sales,inventory],axis=1)
+    h1 = fflayer(p,inp,options={},prefix='qn_1',activ='lambda x: T.nnet.relu(x)')
+    h2 = fflayer(p,h1,options={},prefix='qn_2',activ='lambda x: T.nnet.relu(x)')
+    qv = fflayer(p,h2,options={},prefix='qn_3',activ='lambda x: x')
+
+    return qv
+
+'''
+    State to expected Q-value marginalized over actions.  
+    In this context this is sales, which is an (Nx1) matrix.  
+'''
+def marginalqnetwork(p,sales):
+    inp = sales
+    h1 = fflayer(p,inp,options={},prefix='mqn_1',activ='lambda x: T.nnet.relu(x)')
+    h2 = fflayer(p,h1,options={},prefix='mqn_2',activ='lambda x: T.nnet.relu(x)')
+    qv = fflayer(p,h2,options={},prefix='mqn_3',activ='lambda x: x')
+
+    return qv
+    
+p = init_params({})
+
+tsales_curr = T.matrix()
+tinv_curr = T.matrix()
+tsales_next = T.matrix()
+
+q = qnetwork(p, tsales, tinv)
+mq = marginalqnetwork(p, tsales)
+
+reward = 2.0*tsales - tinv
+
+q_loss = T.mean(T.abs_(q - (mq + reward)))
+
+mq_loss = T.mean(abs_(
+
+'''
+Figure out updates for q and mq.  Both are doing 1-step TD updates.  
+
+'''
+
+run_qnetwork = theano.function([tsales, tinv], outputs = [q])
+run_mqnetwork = theano.function([tsales], outputs = [mq])
+
+if __name__ == "__main__":
+
+    sales = 10.0
+
+    for iteration in range(0,20):
+        inv = basic_policy(sales)
+        sales = basic_environment(inv)
+
+        inv_mat = np.asarray([[inv]]).astype('float32')
+        sales_mat = np.asarray([[sales]]).astype('float32')
+
+        print "qnetwork", run_qnetwork(sales_mat,inv_mat)
+        print "marginal qnetwork", run_mqnetwork(sales_mat)
+
+
+
+
 
 
 
